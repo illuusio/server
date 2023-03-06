a05180745e28be0298245225df65bdc4d3b76852
Otto Kekäläinen <otto@debian.org>
2023-03-05 22:00:50 -0800

    Sync downstream (where applicable) with upstream 10.11 debian/* contents
    Mostly just apply the changes from upstream commit 952af4a1794.

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -17,6 +17,7 @@ set -e
 export DEB_BUILD_OPTIONS="nocheck $DEB_BUILD_OPTIONS"
 
 source ./VERSION
+
 # General CI optimizations to keep build output smaller
 if [[ $GITLAB_CI ]]
 then
@@ -34,7 +35,7 @@ then
   then
     cp -v storage/columnstore/columnstore/debian/mariadb-plugin-columnstore.* debian/
     echo >> debian/control
-    cat storage/columnstore/columnstore/debian/control >> debian/control
+    sed "s/-10.6//" <storage/columnstore/columnstore/debian/control >> debian/control
   fi
 fi
 
@@ -54,17 +55,32 @@ remove_rocksdb_tools()
   fi
 }
 
+add_lsb_base_depends()
+{
+  # Make sure one can run this multiple times remove
+  # lines 'sysvinit-utils' and 'lsb-base'.
+  sed -e '/sysvinit-utils/d' -e '/lsb-base/d' -i debian/control
+  # Add back lsb-base before lsof
+  sed -e 's#lsof #lsb-base (>= 3.0-10),\n         lsof #' -i debian/control
+}
+
 replace_uring_with_aio()
 {
   sed 's/liburing-dev/libaio-dev/g' -i debian/control
-  sed -e '/-DIGNORE_AIO_CHECK=YES/d' \
-      -e '/-DWITH_URING=YES/d' -i debian/rules
+  sed -e '/-DIGNORE_AIO_CHECK=ON/d' \
+      -e '/-DWITH_URING=ON/d' -i debian/rules
 }
 
 disable_pmem()
 {
   sed '/libpmem-dev/d' -i debian/control
-  sed '/-DWITH_PMEM=YES/d' -i debian/rules
+  sed '/-DWITH_PMEM=ON/d' -i debian/rules
+}
+
+disable_libfmt()
+{
+  # 7.0+ required
+  sed '/libfmt-dev/d' -i debian/control
 }
 
 architecture=$(dpkg-architecture -q DEB_BUILD_ARCH)
@@ -91,27 +107,24 @@ fi
 # If not known, use 'unknown' in .deb version identifier
 if [ -z "${LSBID}" ]
 then
-    LSBID="unknown"
+  LSBID="unknown"
 fi
 
 case "${LSBNAME}"
 in
   # Debian
-  stretch)
-    # MDEV-16525 libzstd-dev-1.1.3 minimum version
-    sed -e '/libzstd-dev/d' \
-        -e 's/libcurl4/libcurl3/g' -i debian/control
-    remove_rocksdb_tools
-    disable_pmem
-    ;&
-  buster)
+  "buster")
+    disable_libfmt
     replace_uring_with_aio
     if [ ! "$architecture" = amd64 ]
     then
       disable_pmem
     fi
     ;&
-  bullseye|bookworm)
+  "bullseye")
+    add_lsb_base_depends
+    ;&
+  "bookworm")
     # mariadb-plugin-rocksdb in control is 4 arches covered by the distro rocksdb-tools
     # so no removal is necessary.
     if [[ ! "$architecture" =~ amd64|arm64|ppc64el ]]
@@ -123,19 +136,23 @@ in
       replace_uring_with_aio
     fi
     ;&
-  sid)
+  "sid")
     # The default packaging should always target Debian Sid, so in this case
     # there is intentionally no customizations whatsoever.
     ;;
   # Ubuntu
-  bionic)
+  "bionic")
     remove_rocksdb_tools
     [ "$architecture" != amd64 ] && disable_pmem
     ;&
-  focal)
+  "focal")
     replace_uring_with_aio
+    disable_libfmt
     ;&
-  impish|jammy|kinetic)
+  "jammy"|"kinetic")
+    add_lsb_base_depends
+    ;&
+  "lunar"|"mantic")
     # mariadb-plugin-rocksdb s390x not supported by us (yet)
     # ubuntu doesn't support mips64el yet, so keep this just
     # in case something changes.
@@ -176,17 +193,39 @@ dch -b -D "${LSBNAME}" -v "${VERSION}" "Automatic build with ${LOGSTRING}." --co
 
 echo "Creating package version ${VERSION} ... "
 
+BUILDPACKAGE_DPKGCMD=()
+
+# Fakeroot test
+if fakeroot true; then
+  BUILDPACKAGE_DPKGCMD+=( "fakeroot" "--" )
+fi
+
 # Use eatmydata is available to build faster with less I/O, skipping fsync()
 # during the entire build process (safe because a build can always be restarted)
 if which eatmydata > /dev/null
 then
-  BUILDPACKAGE_PREPEND=eatmydata
+  BUILDPACKAGE_DPKGCMD+=("eatmydata")
+fi
+
+BUILDPACKAGE_DPKGCMD+=("dpkg-buildpackage")
+
+# Using dpkg-buildpackage args
+# -us Allow unsigned sources
+# -uc Allow unsigned changes
+# -I  Tar ignore
+BUILDPACKAGE_DPKGCMD+=(-us -uc -I)
+
+# There can be also extra flags that are appended to args
+if [ -n "$BUILDPACKAGE_FLAGS" ]
+then
+  read -ra BUILDPACKAGE_TMP_ARGS <<< "$BUILDPACKAGE_FLAGS"
+  BUILDPACKAGE_DPKGCMD+=( "${BUILDPACKAGE_TMP_ARGS[@]}" )
 fi
 
 # Build the package
 # Pass -I so that .git and other unnecessary temporary and source control files
 # will be ignored by dpkg-source when creating the tar.gz source package.
-fakeroot $BUILDPACKAGE_PREPEND dpkg-buildpackage -us -uc -I $BUILDPACKAGE_FLAGS
+"${BUILDPACKAGE_DPKGCMD[@]}"
 
 # If the step above fails due to missing dependencies, you can manually run
 #   sudo mk-build-deps debian/control -r -i
@@ -199,7 +238,11 @@ then
   for package in *.deb
   do
     echo "$package" | cut -d '_' -f 1
-    dpkg-deb -c "$package" | awk '{print $1 " " $2 " " $6 " " $7 " " $8}' | sort -k 3
+    # shellcheck disable=SC2034
+    dpkg-deb -c "$package" | while IFS=" " read -r col1 col2 col3 col4 col5 col6 col7 col8
+    do
+        echo "$col1 $col2 $col6 $col7 $col8" | sort -k 3
+    done
     echo "------------------------------------------------"
   done
 fi
